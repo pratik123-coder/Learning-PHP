8de598ec7f84146b6ed4b9f0ef1d6f8d08d297d4
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2023-08-02 09:10:59 +0530

    Working On creating a new form from scratch

--- PHP Form/form1.php
+++ PHP Form/form1.php
@@ -1,16 +1,17 @@
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact Us</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Form</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container">
+<nav class="navbar navbar-expand-lg bg-body-tertiary p-2">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">GET and POST</a>
+        <a class="navbar-brand" href="#">Query Form</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -34,7 +35,7 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
             <form class="d-flex" role="search">
@@ -44,64 +45,61 @@
         </div>
     </div>
 </nav>
-<div class="container pt-3 pb-1">
-<h1>Contact Us For Your Concerns</h1>
-</div>
-<?php
-if ($_SERVER['REQUEST_METHOD']=='POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $desc = $_POST['desc'];
 
-    //Connect to Database
+<!--php-->
+    <?php
+    if ($_SERVER['REQUEST_METHOD']=='POST'){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $database = "contacts";
+        //Connect To Database
 
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    if (!$conn) {
-        die("Sorry we failed to connect");
-    } else {
-        echo "Connection was succesfull";
-    }
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $database = "contacts";
 
+        $conn= mysql_connect($servername,$username,$password,$database);
+        if (!$conn){
+            die("sorry we failed to connect");
+        }
 
-    //Submit to Database
+        //Submit to Database
 
-
-    $sql = "INSERT INTO `contactus` (`name`, `email`, `concern`, `date`) VALUES ('$name',' $email','$desc', CURRENT_TIMESTAMP)";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
-        echo '<div class="alert alert-green alert-dismissible fade show" role="alert"><strong>Success! </strong>
+        $sql="INSERT INTO `contactus2` ('email`, `password`) VALUES ('$email' , '$password')";
+        $res=mysqli_query($conn,$sql);
+        if ($res){
+            echo '<div class="alert alert-green alert-dismissible fade show" role="alert"><strong>Success! </strong>
                 Your entry has been submitted successfully
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                </div>';
-    } else {
-        echo "The Data was not inserted because of " . mysqli_error($conn);
-    }
+        }
+        else{
+            echo "data was not submitted ";
+        }
 
-}
-?>
-<div class="container mt-3 ">
-    <form action="Form_store_data.php" method="post">
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" class="form-control" id="name">
-        </div>
+    }
+    ?>
+
+</div>
+<div class="container ps-4">
+    <form action="form1.php" method="post">
         <div class="mb-3">
             <label for="email" class="form-label">Email address</label>
             <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-            <label for="desc" class="form-label">Description</label>
-            <textarea class="form-control" name="desc" id="desc" cols="30" rows="10"></textarea>
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="password">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
 </body>
 </html>
